cc5897d3d69eee369ae28321b651637aa99e21a4
terauchi <terautikento@gmail.com>
2019-08-15 16:17:50 +0900

    basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
     
   def basic_auth
-    if(ENV["BASIC_AUTH"]!=1)then return end
+    if(ENV["BASIC_AUTH"].to_i!=1)then return end
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
       #username == "user" && password == "pass"
     end
   end
   def basic_auth_admin
-    if(ENV["BASIC_AUTH"]!=1)then return end
+    if(ENV["BASIC_AUTH"].to_i!=1)then return end
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_ADMIN_USER"] && password == ENV["BASIC_AUTH_ADMIN_PASSWORD"]
       #username == "use" && password == "pas"
