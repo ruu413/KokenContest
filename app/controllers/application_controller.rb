feda1952053c47a315fe6ad32c56a978d968962a
terauchi <terautikento@gmail.com>
2019-08-15 16:11:03 +0900

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,17 @@
 class ApplicationController < ActionController::Base
     
   def basic_auth
-    #authenticate_or_request_with_http_basic do |username, password|
-    #  username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    if(ENV["BASIC_AUTH"]!=1)then return end
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
       #username == "user" && password == "pass"
-    #end
+    end
   end
   def basic_auth_admin
-    #authenticate_or_request_with_http_basic do |username, password|
-    #  username == ENV["BASIC_AUTH_ADMIN_USER"] && password == ENV["BASIC_AUTH_ADMIN_PASSWORD"]
+    if(ENV["BASIC_AUTH"]!=1)then return end
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_ADMIN_USER"] && password == ENV["BASIC_AUTH_ADMIN_PASSWORD"]
       #username == "use" && password == "pas"
-    #end
+    end
   end
 end
